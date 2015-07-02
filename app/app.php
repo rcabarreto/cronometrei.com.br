f8f6e4d5af128afbc00d4170c4d0b72e5a0f73e9
Rodrigo Barreto <rcabarreto@gmail.com>
2015-07-02 18:41:41 -0300

    all

--- app/app.php
+++ app/app.php
@@ -5,15 +5,15 @@
 	<meta name="description" content="Cronômetro online">
 	<meta name="keywords" content="Cronômetro, Contador, Relógio, Tempo, Timer, Cronometer">
 	<meta name="author" content="R3 Web Solutions">
-	<title>Cronometrei</title>
+	<title>00:00:000 - Cronometrei</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 </head>
 <body>
 
-<div id="timer">00:00:000</div>
+
 
 <script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="assets/js/jquery.hotkeys.min.js"></script>
-<script type="text/javascript" src="assets/js/functions.dev.js"></script>
+<script type="text/javascript" src="assets/js/functions.dev.oo.js"></script>
 </body>
 </html>
