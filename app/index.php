dd7d0154af7d13039672005ac1032c46b3172adb
Rodrigo Barreto <rcabarreto@gmail.com>
2015-07-09 10:35:07 -0300

    changes

--- app/index.php
+++ app/index.php
@@ -35,8 +35,8 @@
 
 	<title></title>
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,13 +56,21 @@
 	</header>
 
 	<!-- app container -->
-	<div id="application" class="container"></div>
+	<div id="application" class="container">
+		<div id="titleRow" class="row">
+			<h1 id="appTitle">Cronometrei</h1><div id="timer" class="col-md-8 col-md-offset-2">00:00:00:000</div>
+		</div>
+		<div id="controlRow" class="row">
+			<div id="startStop" class="button col-md-2 col-md-offset-3" onclick="app.startStopTimer();"><div id="startStopLabel"></div><div id="startStopInstruction" class="instructions"></div></div>
+			<div id="clear" class="button col-md-2 col-md-offset-2" onclick="app.clearTimer();"><div id="clearLabel"></div><div id="clearInstruction" class="instructions"></div></div>
+		</div>
+	</div>
 
 	<!-- footer -->
 	<footer></footer>
 
 	<script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
-	<script type="text/javascript" src="assets/js/functions.min.js"></script>
+	<script type="text/javascript" src="assets/js/functions.dev.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
