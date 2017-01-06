91def13d170354f546a36aba514f0858d1e7001d
Rodrigo Barreto <rcabarreto@gmail.com>
2017-01-06 15:16:02 -0200

    Dev of api

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var path = require('path');
+var helmet = require('helmet');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
-var _ = require('underscore');
-
 var db = require('./db.js');
 var middleware = require('./middleware.js')(db);
 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api')(db, middleware);
 
 var app = express();
+
+// configure headers
+app.use(helmet());
+app.disable('x-powered-by');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, Auth, X-Requested-With");
+//     res.header("Access-Control-Expose-Headers", "Authorization, Auth");
+//     res.header('Access-Control-Allow-Credentials', true);
+//     next();
+// });
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,27 +46,18 @@ app.use(require('node-sass-middleware')({
     indentedSyntax: true,
     sourceMap: true
 }));
+
+// setup static files access
 app.use(express.static(path.join(__dirname, 'public')));
 
+// mount the home page
 app.use('/', index);
+
+// mount the users page
 app.use('/users', users);
 
-
-
-
-
-
-// TODO: paste api calls here
-
-app.get('/api', function(req, res){
-    res.send('Welcome to Contability API!');
-});
-
-app.get('/api/user', middleware.requireAuthentication, function(req, res){
-    res.send('Welcome to Contability API!');
-});
-
-
+// mount the api
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -72,12 +77,11 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-
+// sync the database
 db.sequelize.sync({force:true}).then(function() {
     var themes = [ {theme_name: "London", image_name: "london.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Borabora", image_name: "borabora.jpg", logo_color: "#F4FCFA", active: 1}, {theme_name: "Bubbles", image_name: "bubbles.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Road", image_name: "road.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Squares", image_name: "150305-cinqAA_by_Pierre_Cante.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Snow", image_name: "11220682974_9d296080f3_k.jpg", logo_color: "##E7E8EB", active: 1}, {theme_name: "Sunlight", image_name: "11416120446_76a5ae1b18_k.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Boat", image_name: "12591084605_c926ed2c7d_k.jpg", logo_color: "#FFF", active: 1}, {theme_name: "Balloons", image_name: "12735618625_bbe342c702_k.jpg", logo_color: "#587065", active: 1}, {theme_name: "Christmas Lights", image_name: "Christmas_Lights_by_RaDu_GaLaN.jpg", logo_color: "#FFF", active: 1} ];
     db.theme.bulkCreate(themes).then(function (themes) {}, function (e) {});
 });
 
-
-
+// export the module
 module.exports = app;
