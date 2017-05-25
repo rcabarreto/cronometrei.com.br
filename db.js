d4474c36679e4ac4e8f5ba93b3118f863156256b
Rodrigo Barreto <rcabarreto@gmail.com>
2017-05-25 16:23:58 -0300

    setting sqlite as default database engine for now

--- db.js
+++ db.js
@@ -5,16 +5,16 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env === 'production'){
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
-    });
-}else{
-    sequelize = new Sequelize(undefined, undefined, undefined, {
-        'dialect': 'sqlite',
-        'storage': __dirname + '/data/dev-cronometrei-api.sqlite'
-    });
-}
+// if(env === 'production'){
+//     sequelize = new Sequelize(process.env.DATABASE_URL, {
+//         dialect: 'postgres'
+//     });
+// }else{
+sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/dev-cronometrei-api.sqlite'
+});
+// }
 
 var db = {};
 
